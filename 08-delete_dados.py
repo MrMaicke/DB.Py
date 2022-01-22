3006789b2fa070e79360800a73239dcb5283d73f
marcelo <marcelosantiago944@gmail.com>
2022-01-22 20:16:42 -0300

    delete_dados

---
+++ 08-delete_dados.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("clientes.db")
+cursor = conn.cursor()
+
+id_cliente = 8
+
+cursor.execute("""
+DELETE FROM clientes
+WHERE id = ?
+""", (id_cliente,))
+
+conn.commit()
+
+print('Registro excluido com sucesso.')
+
+conn.close()
