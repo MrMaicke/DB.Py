1ff313236fd9fe4789c76f16620f16eeda672526
marcelo <marcelosantiago944@gmail.com>
2022-01-22 17:20:35 -0300

    coreção de nomenclatura e read

---
+++ 06-lendo_dados.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("clientes.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+SELECT * FROM clientes
+""")
+
+for linha in cursor.fetchall():
+    print(linha)
+
+conn.close()
